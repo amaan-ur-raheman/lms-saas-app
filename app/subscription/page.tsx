626463028e53c480d441e2165bc8ea2631aa52ad
Amaan Ur Raheman <amaanurrahemanshaikh@gmail.com>
2025-07-14 23:41:01 +0530

    feat: Add clerk subscription check

--- app/subscription/page.tsx
+++ app/subscription/page.tsx
@@ -3,9 +3,9 @@ import React from 'react'
 
 const Subscription = () => {
   return (
-    <div className='flex items-center justify-center h-full mt-10 mx-auto min-h-full px-10 my-auto'>
+    <main>
       <PricingTable />
-    </div>
+    </main>
   )
 }
 
